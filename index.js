0a1430c8b5946e037a9417b77cbc3e75452d81a1
Samuel Amity <sam.amity@gmail.com>
2022-07-04 20:06:08 -0400

    commit tree a896060eeab4d2bd3d870eca7cc228d288e7ac25

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+function saturdayFun (activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+};
+
+function mondayWork (work = "go to the office") {
+    return `This Monday, I will ${work}.`
+};
+
+function wrapAdjective (adjective = `special`) {
+    return function encouragingPrompt (emphatic = `*`) {
+        return `You are ${adjective}${emphatic}${adjective}!`
+    }; 
+};
